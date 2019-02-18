ec984cd5818f9cb32d2b1be9cb07b12ec9b7a036
dlaredo <davidlaredo1@gmail.com>
2019-02-18 14:43:31 -0800

    Adding mse and accuracy to custom scores

--- custom_scores.py
+++ custom_scores.py
@@ -26,7 +26,21 @@ def root_mean_squared_error(y_true, y_pred):
 
     return s
 
-score_dict = {'rhs':lambda x,y : rul_health_score(x,y), 'rmse':lambda x,y : root_mean_squared_error(x,y)}
+def accuracy(y_true, y_pred):
+
+    argmax_true = np.argmax(y_true, axis=1)
+    argmax_false = np.argmax(y_pred, axis=1)
+
+    aux = argmax_true - argmax_false
+    accurate = [1 if e==0 else 0 for e in aux]
+
+    accuracy = np.mean(accurate)
+
+    return accuracy
+
+
+score_dict = {'rhs':lambda x,y : rul_health_score(x,y), 'rmse':lambda x,y : root_mean_squared_error(x,y),
+              'mse':lambda x,y : mean_squared_error(x,y), 'accuracy':lambda x, y : accuracy(x, y)}
 
 def compute_score(score_name, y_true, y_pred):
     
